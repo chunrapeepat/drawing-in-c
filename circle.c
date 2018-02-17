68e5721b32faf134c13068430c6af909083a52e3
chunza2542 <chunza2542@hotmail.com>
2018-02-17 14:59:01 +0700

    render basic circle

--- circle.c
+++ circle.c
@@ -1,23 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+
+void rgb(int r, int g, int b, FILE *fp) {
+    static unsigned char color[3];
+    color[0] = r;  /* red */
+    color[1] = g;  /* green */
+    color[2] = b;  /* blue */
+    (void) fwrite(color, 1, 3, fp);
+}
 
 int main(void)
 {
-  const int dimx = 800, dimy = 800;
+  const int dimx = 1000, dimy = 1000;
+  const int circle_d = 300;
   int i, j;
+
   FILE *fp = fopen("image.ppm", "wb");
   (void) fprintf(fp, "P6\n%d %d\n255\n", dimx, dimy);
+
+  // start drawing
   for (j = 0; j < dimy; ++j)
   {
     for (i = 0; i < dimx; ++i)
     {
-      static unsigned char color[3];
-      color[0] = 0;  /* red */
-      color[1] = 255;  /* green */
-      color[2] = 255;  /* blue */
-      (void) fwrite(color, 1, 3, fp);
+        if (pow(i - dimx / 2, 2) + pow(j - dimy / 2, 2) <= pow(circle_d, 2)) {
+            rgb(255, 0, 0, fp);
+        } else {
+            rgb(255, 255, 255, fp);
+        }
     }
   }
+
   (void) fclose(fp);
   return EXIT_SUCCESS;
 }
